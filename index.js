99e6908f2a5d3069356cd01571a922e321f96b59
lawrahkonwea <lawrahcollections@gmail.com>
2023-08-11 14:15:15 +0100

    Add new  image for card

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const projectList = [
     li2: 'Frontend dev',
     li3: '2023',
     id: 1,
-    project_image: 'images/leaderboard.png',
+    project_image: 'images/leader.jpg',
     source_icon: 'images/github.jpg',
     live_icon: 'images/Vector.png',
     description: "This is JavaScript project for the Leaderboard list app, using webpack and ES6 features. API class with adding and removing(A create function for id) respective methods for showing data in Dom refresh function.",
